188cea46d7dd80492d233ff4ea16dba2297d1923
Ronnie R. Winter <ro2nie@gmail.com>
2020-01-19 01:09:17 +0000

    Refactors some of the logic.

--- float-switch-ultrasonic-sensor/float-switch-ultrasonic-sensor.ino
+++ float-switch-ultrasonic-sensor/float-switch-ultrasonic-sensor.ino
@@ -16,10 +16,16 @@ unsigned long timeStarted = 0;
 bool pumpState = false;
 String waterTankIntent = "OFF";
 String delayStart = "";
-int delayStartInt = 12000; //Debounce for when well signals enough water. Pump needs to wait or else it wil stop/start in quick succession.
+int delayStartInt = 1800000; //Half hr - Debounce for when well signals enough water. Pump needs to wait or else it wil stop/start in quick succession.
 int minuteCountdown = 0;
 
-void setup_wifi() {
+const char* timeToWaitTopic = "water-well/time-to-wait";
+const char* waterTankIntentTopic = "water-tank/status";
+const char* waterPumpStatusTopic = "water-pump/status";
+const char* waterPumpControllerStatusTopic = "water-pump/controller/status";
+const char* waterWellTimeTopic = "water-well/time";
+
+void setupWifi() {
   delay(10);
   // Start by connecting to a WiFi network
   Serial.println("Connecting to: " + String(ssid));
@@ -35,27 +41,21 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.println("] ");
-  const char* timeToWaitTopic = "water-well/time-to-wait";
-  const char* waterTankIntentTopic = "water-tank/status";
-  
+  Serial.print("Message arrived [" + String(topic) + "]");
   if (strcmp(topic, waterTankIntentTopic) == 0) {
-    Serial.println("TODO-0");
     waterTankIntent = "";
     for (int i = 0; i < length; i++) {
       waterTankIntent += (char)payload[i];
     }
-    Serial.println("Received water-tank/status" + waterTankIntent);
+    Serial.println("Received " + String(waterTankIntentTopic) + " " + waterTankIntent);
   } else if (strcmp(topic, timeToWaitTopic) == 0) {
     delayStart = "";
     for (int i = 0; i < length; i++) {
       delayStart += (char)payload[i];
     }
-    delayStartInt = delayStart.toInt() * 60 * 1000;
-    client.publish("water-well/time", delayStart.c_str());
-    Serial.println("Received message water-well/time-to-wait" + delayStart);
+    delayStartInt = delayStart.toInt() * 60000;
+    client.publish(waterWellTimeTopic, delayStart.c_str());
+    Serial.println("Received " + String(timeToWaitTopic) + " " + delayStart);
   }
 }
 
@@ -69,11 +69,11 @@ void reconnect() {
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
-      client.subscribe("water-tank/status");
-      client.subscribe("water-well/time-to-wait");      
-      client.publish("water-pump/controller/status", "OFF");
+      client.subscribe(waterTankIntentTopic);
+      client.subscribe(timeToWaitTopic);
+      client.publish(waterPumpControllerStatusTopic, "OFF");
       delay(500);
-      client.publish("water-pump/controller/status", "ON");
+      client.publish(waterPumpControllerStatusTopic, "ON");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -91,11 +91,11 @@ void setup() {
   digitalWrite(LED_BUILTIN, HIGH);  //Start light as off. High is use because current needs to flow through, to turn off
 
   Serial.begin(9600); //Open a serial port.
-  setup_wifi();
+  setupWifi();
   client.setServer(mqttServer, 1883);
   client.setCallback(callback);
   Serial.println("About to set initial waiting time");
-  Serial.println(delayStartInt);  
+  client.publish(waterWellTimeTopic, delayStart.c_str());
 }
 
 void loop() {
@@ -107,49 +107,41 @@ void loop() {
   if (digitalRead(WELL_LEVEL_SENSOR) == HIGH)  //If the water well level is low
   {
     timeStopped = millis();
-
     if (pumpState) {  //And the pump is already running
-      pumpState = false;  //Then switch it off
-      Serial.print("Pump OFF at: " + String(timeStopped) + "\n");
-      digitalWrite(LED_BUILTIN, HIGH);
-      client.publish("water-pump/status", "OFF");
-      client.publish("water-well/time", delayStart.c_str());
-      digitalWrite(RELAY_SWITCH, LOW);
+      setPumpState(false, delayStart);
     }
   }
   else //If there is enough water in the well
   {
     timeStarted = millis();
-
     //If the pump is running
     if (pumpState) {
       if (waterTankIntent == "OFF" || waterTankIntent != "ON") { //Water tank is full. If anything else other than "ON or OFF" is sent. Switch off
-        pumpState = false;  //Then switch it off
-        Serial.println("Pump OFF");
-        digitalWrite(LED_BUILTIN, HIGH);
-        client.publish("water-pump/status", "OFF");        
-        client.publish("water-well/time", delayStart.c_str());
-        digitalWrite(RELAY_SWITCH, LOW);
+        setPumpState(false, "0");
       }
     } else { //if pump not running
-      if (waterTankIntent == "ON") { //Water tank is empty
-        if ((timeStarted - timeStopped) >= delayStartInt) { //And the delay counter is over, then switch pump on
-          pumpState = true;
-          Serial.println("Pump ON");
-          digitalWrite(LED_BUILTIN, LOW);
-          client.publish("water-pump/status", "ON");
-          client.publish("water-well/time", String(minuteCountdown).c_str());
-          digitalWrite(RELAY_SWITCH, HIGH);
-        } else { //Send how long it will be until pump started every minute
-          int timeToWait = ((delayStartInt -(timeStarted - timeStopped)) / 60000);
-          if (timeToWait != minuteCountdown) {
-            minuteCountdown = timeToWait;
-            String countdown = String(minuteCountdown + 1);
-            Serial.println("Starting in: " + countdown + " minutes");
-            client.publish("water-well/time", countdown.c_str());            
-          }
+      if ((timeStarted - timeStopped) >= delayStartInt) { //And the delay counter is over, then switch pump on
+        if (waterTankIntent == "ON") { //Water tank is empty
+          setPumpState(true, "0");
+        }
+      } else { //Send how long it will be until pump started every minute
+        int timeToWait = ((delayStartInt - (timeStarted - timeStopped)) / 60000);
+        if (timeToWait != minuteCountdown) {
+          minuteCountdown = timeToWait;
+          Serial.println("Starting in: " + String(minuteCountdown) + " minutes");
+          client.publish(waterWellTimeTopic, String(minuteCountdown).c_str());
         }
       }
     }
   }
+}
+
+void setPumpState(bool state, String wellTimeToRecover) {
+  pumpState = state;
+  const String stateStr = state ? "ON" : "OFF";
+  Serial.println("Pump " + stateStr);
+  digitalWrite(LED_BUILTIN, (state ? LOW : HIGH));
+  client.publish(waterPumpStatusTopic, stateStr.c_str());
+  client.publish(waterWellTimeTopic, wellTimeToRecover.c_str());
+  digitalWrite(RELAY_SWITCH, (state ? HIGH : LOW));
 }
